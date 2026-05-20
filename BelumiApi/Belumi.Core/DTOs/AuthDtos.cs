38a7a10b29e498ea34f75cf569e020fa84076000
DuowTH2208 <duonghtse180223@fpt.edu.vn>
2026-05-20 23:19:20 +0700

    Resolve AuthDtos conflict

--- BelumiApi/Belumi.Core/DTOs/AuthDtos.cs
+++ BelumiApi/Belumi.Core/DTOs/AuthDtos.cs
@@ -2,11 +2,11 @@ using Belumi.Core.Entities;
 
 namespace Belumi.Core.DTOs;
 
-<<<<<<< Updated upstream
+
 public sealed record LoginRequest(string Email, string Password);
 public sealed record RegisterRequest(string Email, string Password, string FullName, string? Phone);
 public sealed record FirebaseLoginRequest(string? IdToken, string? FirebaseUid, string? Email, string? FullName, string? AvatarUrl);
-=======
+
 public sealed record FirebaseLoginRequest(string IdToken);
->>>>>>> Stashed changes
+
 public sealed record AuthResponse(Guid UserId, string Email, string FullName, string? Phone, UserRole Role, string Token);
