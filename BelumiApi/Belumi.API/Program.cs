9a262fcc4b2465b82726f0e011f03fc6359b62f7
James Nguyen <165425332+huuanh20@users.noreply.github.com>
2026-05-31 23:39:53 +0700

    ci: setup aws beanstalk deployment and postgres rds

--- BelumiApi/Belumi.API/Program.cs
+++ BelumiApi/Belumi.API/Program.cs
@@ -42,7 +42,7 @@ var app = builder.Build();
 
 app.UseExceptionHandler();
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
